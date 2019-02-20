e4171a957404eb0acb41637e1b2867a1bc4cc3df
joey <joehaiyi@hotmail.com>
2019-02-19 23:04:13 -0500

    Initial commit

---
+++ app/src/main/java/com/elec390coen/alcoroam/Activities/Main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.elec390coen.alcoroam.Activities.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.elec390coen.alcoroam.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.style_activity_main);
+    }
+}
