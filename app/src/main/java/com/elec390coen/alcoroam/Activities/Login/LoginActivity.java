3dc4aa66d69dfba7f3c4c1da846acfb1dce3f3f9
joey <joehaiyi@hotmail.com>
2019-02-20 16:00:29 -0500

    Added textview on login screen

--- app/src/main/java/com/elec390coen/alcoroam/Activities/Login/LoginActivity.java
+++ app/src/main/java/com/elec390coen/alcoroam/Activities/Login/LoginActivity.java
@@ -2,14 +2,17 @@ package com.elec390coen.alcoroam.Activities.Login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.elec390coen.alcoroam.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.style_activity_login);
+
     }
 }
