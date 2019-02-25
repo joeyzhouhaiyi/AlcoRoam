dbf51ff2475bad8ae505414ead5a971fb50630d1
joey <joehaiyi@hotmail.com>
2019-02-24 20:16:41 -0500

    Log in screen UI created

--- app/src/main/java/com/elec390coen/alcoroam/Activities/Login/LoginActivity.java
+++ app/src/main/java/com/elec390coen/alcoroam/Activities/Login/LoginActivity.java
@@ -2,19 +2,33 @@ package com.elec390coen.alcoroam.Activities.Login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.elec390coen.alcoroam.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText et_name;
+    private EditText et_pass;
+    private Button btn_login;
+    private Button btn_signup;
 
-    TextView t21343543534;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.style_activity_login);
+    }
 
+    private void initView()
+    {
+        /*
+        et_name = findViewById(R.id.et_name);
+        et_pass = findViewById(R.id.et_pass);
+        btn_login = findViewById(R.id.btn_login);
+        btn_signup = findViewById(R.id.btn_signup);
+        */
     }
 }
