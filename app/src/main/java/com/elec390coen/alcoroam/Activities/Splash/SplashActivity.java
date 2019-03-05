d271bef6988bfba97ef71cf785e9f1eed1a63c69
Kevin <chenzy.kevin@gmail.com>
2019-03-04 21:40:01 -0500

    splash done

--- app/src/main/java/com/elec390coen/alcoroam/Activities/Splash/SplashActivity.java
+++ app/src/main/java/com/elec390coen/alcoroam/Activities/Splash/SplashActivity.java
@@ -2,14 +2,24 @@ package com.elec390coen.alcoroam.Activities.Splash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.elec390coen.alcoroam.R;
 
 public class SplashActivity extends AppCompatActivity {
-
+private TextView tv;
+private ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.style_activity_splash);
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
     }
 }
